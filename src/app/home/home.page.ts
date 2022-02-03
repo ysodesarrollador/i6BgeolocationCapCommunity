81eda73c09a56536f0051efbfa983f0f8d9d139f
yso <ysodesarrollador@gmail.com>
2022-02-02 21:48:44 -0500

    update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,20 +6,8 @@ import { BGService } from '../services/bg.service';
 import { Geolocation } from '@capacitor/geolocation';
 import { LocalNotifications } from '@capacitor/local-notifications';
 
-
 const BackgroundGeolocation = registerPlugin<BackgroundGeolocationPlugin>("BackgroundGeolocation");
-const started = Date.now();
-const watcher_colours = {};
-const colours = [
-    "red",
-    "green",
-    "blue",
-    "yellow",
-    "pink",
-    "orange",
-    "purple",
-    "cyan"
-];
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -29,205 +17,63 @@ export class HomePage implements OnInit{
  
     locations: any = [];
     watch: string;
-    newlat: number;
-    newlng: number;
+    newlat: number = 0;
+    newlng: number = 0;
     starbg = false;
     activateWatch: boolean = true;
     id: any;
+    distanceFilter: number = 10;
 
   constructor(
     private zone: NgZone,
     private bg: BGService,
     private plt: Platform
   ) {}
-  ngOnInit(): void {
+    ngOnInit(): void {
 
-    this.bg.$starbg.subscribe(resp =>{
-        this.starbg = resp;
-    })
-/*     this.bg.$storeLocation.subscribe(resp =>{
-        this.locations = resp;
-        console.log(this.locations.length)
-    }) */
-
-try {
-   if(this.plt.is('hybrid') ){
-    Geolocation.requestPermissions();
-   }
-   
-} catch (error) {
-   
-}
-/*     this.motionServ.$acceleration.subscribe(acel =>{
-    if (!acel)
-        return;
-    this.x = acel.x;
-    this.y = acel.y;
-    this.z = acel.z;      
-    if(this.x > 1 || this.y > 1)  {
-        alert(this.x + " " + this.x)
-    }         
-}) */
-}
-
-   log_for_watcher(text , time, colour = "gray") {
-       return
-    const li = document.createElement("li");
-    li.style.color = colour;
-    li.innerText = String(Math.floor(( started) / 1000)) + ":" + text;
-    const container = document.getElementById("log");
-    return container.insertBefore(li, container.firstChild);
-}
-
- log_error(error, colour = "gray") {
-    console.error(error);
-    return this.log_for_watcher(
-        error.name + ": " + error.message,
-        Date.now(),
-        colour
-    );
-}
-
- log_location(location, watcher_ID) {
-    return this.log_for_watcher(
-        location.latitude + ":" + location.longitude,
-        location.time,
-        watcher_colours[watcher_ID]
-    );
-}
-
- add_watcher(background) {
-     this.starbg = true;
-    let id;
-    BackgroundGeolocation.addWatcher(
-        Object.assign({
-            stale: true
-        }, (
-            background
-            ? {
-                backgroundTitle: "Tracking your location, senõr.",
-                backgroundMessage: "Cancel to prevent battery drain."
-            }
-            : {
-                distanceFilter: 50
-            }
-        )),
-        (location, error)=>{
-         {
-            if (error) {
-                if (
-                    error.code === "NOT_AUTHORIZED" &&
-                    window.confirm(
-                        "This app needs your location, " +
-                        "but does not have permission.\n\n" +
-                        "Open settings now?"
-                    )
-                ) {
-                    BackgroundGeolocation.openSettings();
-                }
-                return this.log_error(error, watcher_colours[id]);
-            }
-            this.zone.run(()=>{
-                this.locations.push(location);
-            })
-           
-            LocalNotifications.schedule({
-                notifications: [
-                  {
-                    id:111,
-                    title:"Está conectado a Internet",
-                    body:"Background Geolocation Deshabilitdado",            
-                    extra: {
-                      data:'localNotificationActionPerformed'
-                    }
-                  }
-                  ]
-              });
-              this.id = id;
-              //BackgroundGeolocation.removeWatcher(this.id)
-            return  true;//this.log_location(location, id);
+        try {
+        if(this.plt.is('hybrid') ){
+            Geolocation.requestPermissions();
         }
-        }
-    )
-}
-
-// Produces the most accurate location possible within the specified time limit.
- make_guess(timeout) {
-    return new Promise(function (resolve) {
-        let last_location = null;
-        let id;
-        BackgroundGeolocation.addWatcher(
-            {
-                requestPermissions: false,
-                stale: true
-            },
-            function callback(location) {
-                last_location = location;
-            }
-        ).then(function retain_callback_id(the_id) {
-            id = the_id;
-        });
-
-        setTimeout(function () {
-            resolve(last_location);
-            BackgroundGeolocation.removeWatcher({id});
-        }, timeout);
-    });
-}
-
- guess(timeout) {
-    return this.make_guess(timeout).then(function (location:any) {
-        return (
-            location === null
-            ? this.log_for_watcher("null", Date.now())
-            : this.log_for_watcher(
-                [
-                    location.latitude,
-                    location.longitude
-                ].map(String).join(":"),
-                location.time
-            )
-        );
-    });
-  }
-
-      /** Activar o desactivar mi localización */
-      async toogleMyLocation(){    
-        if(this.activateWatch){
-            this.activateWatch = !this.activateWatch ;
-            var options: PositionOptions ={
-                enableHighAccuracy:true
-            }      
         
-            this.watch = await Geolocation.watchPosition(options,(position, err)=>{
-                this.zone.run(() => {
-                if(position){               
-                    this.newlat = position.coords.latitude ;
-                    this.newlng = position.coords.longitude ;                
-                }
-            });
-            console.log("this.watch") 
-            console.log(JSON.stringify(this.watch))      
-            
-        })
+        } catch (error) {
+        
+            }
         }
-        else{
-            Geolocation.clearWatch({id:this.watch})
-            this.activateWatch = !this.activateWatch ;
-        }
+    /** Activar o desactivar mi localización */
+    async toogleMyLocation(){    
+    if(this.activateWatch){
+        this.activateWatch = !this.activateWatch ;
+        var options: PositionOptions ={
+            enableHighAccuracy:true
+        }      
+
+        this.watch = await Geolocation.watchPosition(options,(position, err)=>{
+            this.zone.run(() => {
+            if(position){               
+                this.newlat = position.coords.latitude ;
+                this.newlng = position.coords.longitude ;                
+            }
+        });
+        console.log("this.watch") 
+        console.log(JSON.stringify(this.watch))      
+        
+    })
+    }
+    else{
+        Geolocation.clearWatch({id:this.watch})
+        this.activateWatch = !this.activateWatch ;
+    }
     }
 
     stop(){
         this.zone.run(()=>{
             this.starbg = false;
-            console.log("this.id: ")
-            console.log(this.id)
-             BackgroundGeolocation.removeWatcher(this.id)
         })
     }
 
     /* Otherrrrr */
-    add_watcher1(){
+    start(){
         this.starbg = true;
         BackgroundGeolocation.addWatcher(
             {
@@ -257,7 +103,7 @@ try {
         
                 // The minimum number of metres between subsequent locations. Defaults
                 // to 0.
-                distanceFilter: 50
+                distanceFilter: this.distanceFilter
             },
             ((location, error)=> {
                 if (error) {
@@ -291,27 +137,35 @@ try {
                       }
                       ]
                   });
+                  if(this.starbg == false){
+                    try {
+                        BackgroundGeolocation.removeWatcher({
+                            id: this.id
+                        });
+                    } catch (error) {
+                        alert(error)
+                    }
+                    this.starbg = true;
+                }
                 //return console.log(location);
             }
         )).then((watcher_id)=>{
-             {
+            this.zone.run(()=>{
                 console.log("watcher_id es: ")
-                 console.log(watcher_id)
+                console.log(watcher_id)
                 this.id = watcher_id;
                 // When a watcher is no longer needed, it should be removed by calling
                 // 'removeWatcher' with an object containing its ID.
-/*                 setTimeout(() => {
-                    BackgroundGeolocation.removeWatcher({
-                        id: watcher_id
-                    }); 
-                }, 5000); */
-            }
+/*                 if(this.starbg == false){
+                    setTimeout(() => {
+                        BackgroundGeolocation.removeWatcher({
+                            id: watcher_id
+                        }); 
+                        this.starbg = true;
+                    }, 100);
+                } */
+            })
         } );
-        
-        // If you just want the current location, try something like this. The longer
-        // the timeout, the more accurate the guess will be. I wouldn't go below about
-        // 100ms.
-
     }
 }
 
